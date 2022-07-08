b4078db10fbb2eba47b4f164ff87adfd2f737ea8
Neco86 <33189641+Neco86@users.noreply.github.com>
2022-07-08 16:33:37 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 rm -rf ./dist
 mkdir dist
-cp index.html ./dist/index.html
-yarn uglifycss ./index.css --expand-vars --output ./dist/index.min.css
-yarn uglifyjs index.js  -cm -o ./dist/index.min.js
-yarn uglifyjs inject.js -cm -o ./dist/inject.min.js
-yarn uglifyjs rulers.js  -cm -o ./dist/rulers.min.js
-yarn uglifyjs sketchMeasure.js -cm -o ./dist/sketchMeasure.min.js
-grep -rl '.css' ./dist  | xargs sed -i "" "s/.css/.min.css/g"
-grep -rl '.js' ./dist  | xargs sed -i "" "s/.js/.min.js/g"
+cp ./src/index.html ./dist/index.html
+yarn uglifycss ./src/index.css --expand-vars --output ./dist/index.min.css
+yarn uglifyjs ./src/index.js  -cm -o ./dist/index.min.js
+yarn uglifyjs ./src/inject.js -cm -o ./dist/inject.min.js
+yarn uglifyjs ./src/rulers.js  -cm -o ./dist/rulers.min.js
+yarn uglifyjs ./src/sketchMeasure.js -cm -o ./dist/sketchMeasure.min.js
