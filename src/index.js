c3aa232e86f75235dd9e4401a81549082b5e145a
Neco86 <33189641+Neco86@users.noreply.github.com>
2022-07-08 16:34:42 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-
+(() => {
+    const uploader = document.getElementById('uploader');
+    const iframe = document.getElementById('iframe');
+    const placeholder = document.getElementById('placeholder');
+    const clear = document.getElementById('clear');
+    let fileList = [];
+    uploader.onchange = ({ target: { files } }) => {
+        if (!files.length) {
+            return;
+        }
+        fileList = [...files].map((file) => ({
+            name: file.name,
+            path: file.webkitRelativePath.replace(/^.*?\//, ''),
+            url: URL.createObjectURL(file),
+        }));
+        const index = fileList.find((t) => t.name === 'index.html');
+        if (index) {
+            fetch(index.url)
+                .then((res) => res.text())
+                .then((res) => {
+                    fileList.forEach((file) => {
+                        const path = file.path
+                            .split('/')
+                            .map(encodeURIComponent)
+                            .join('/');
+                        res = res.replace(path, `${file.url}#`);
+                    });
+                    res = res.replace(/\.\/blob/g, 'blob');
+                    return res;
+                })
+                .then((res) => {
+                    res = res.replace(
+                        '</head>',
+                        `<script src="https://neco86.github.io/Sketch-Measure-Compare/dist/sketchMeasure.min.js"><\/script></head>`
+                    );
+                    return res;
+                })
+                .then((res) => {
+                    const blob = new Blob([res], { type: 'text/html' });
+                    const url = URL.createObjectURL(blob);
+                    iframe.src = url;
+                    placeholder.style.display = 'none';
+                    iframe.style.display = 'block';
+                    clear.style.display = 'block';
+                });
+        }
+    };
+    const handleClear = () => {
+        URL.revokeObjectURL(iframe.src);
+        fileList.forEach((file) => {
+            URL.revokeObjectURL(file.url);
+        });
+        fileList = [];
+        iframe.src = '';
+        uploader.value = '';
+        placeholder.style.display = 'block';
+        iframe.style.display = 'none';
+        clear.style.display = 'none';
+    };
+    document.addEventListener('beforeunload', handleClear);
+    clear.onclick = handleClear;
+})();
