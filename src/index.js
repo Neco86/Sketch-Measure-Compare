3ccc855734daa369749d38048d1312baae20ba18
Neco86 <33189641+Neco86@users.noreply.github.com>
2022-07-13 12:31:25 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ const sketchMeasureCompare = {};
 if (!window.top.sketchMeasureCompare) {
     window.top.sketchMeasureCompare = sketchMeasureCompare;
 
-    sketchMeasureCompare.__proto__.rulers = rulers;
-    sketchMeasureCompare.__proto__.sketchMeasure = sketchMeasure;
-    sketchMeasureCompare.__proto__.js = js;
-    sketchMeasureCompare.__proto__.css = css;
-    sketchMeasureCompare.__proto__.html = html;
+    sketchMeasureCompare.rulers = rulers;
+    sketchMeasureCompare.sketchMeasure = sketchMeasure;
+    sketchMeasureCompare.js = js;
+    sketchMeasureCompare.css = css;
+    sketchMeasureCompare.html = html;
 
     sketchMeasureCompare.init = init;
 
