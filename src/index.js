d1e881eccd307de8467fe34a133b450920efae20
Neco86 <33189641+Neco86@users.noreply.github.com>
2022-07-13 11:56:20 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,69 +1,25 @@
-(() => {
-    const uploader = document.getElementById('uploader');
-    const iframe = document.getElementById('iframe');
-    const placeholder = document.getElementById('placeholder');
-    const clear = document.getElementById('clear');
-    let fileList = [];
-    uploader.onchange = ({ target: { files } }) => {
-        if (!files.length) {
-            return;
-        }
-        fileList = [...files];
-        const isIncludeIndex = !!fileList.find((t) => t.name === 'index.html');
-        if (!isIncludeIndex) {
-            uploader.value = '';
-            alert('UI: index.html Not Found!');
-            return;
-        }
-        fileList = fileList.map((file) => ({
-            name: file.name,
-            path: file.webkitRelativePath.replace(/^.*?\//, ''),
-            url: URL.createObjectURL(file),
-        }));
-        const index = fileList.find((t) => t.name === 'index.html');
-        if (index) {
-            fetch(index.url)
-                .then((res) => res.text())
-                .then((res) => {
-                    fileList.forEach((file) => {
-                        const path = file.path
-                            .split('/')
-                            .map(encodeURIComponent)
-                            .join('/');
-                        res = res.replace(path, `${file.url}#`);
-                    });
-                    res = res.replace(/\.\/blob/g, 'blob');
-                    return res;
-                })
-                .then((res) => {
-                    res = res.replace(
-                        '</head>',
-                        '<script src="https://neco86.github.io/Sketch-Measure-Compare/sketchMeasure.min.js"></script></head>'
-                    );
-                    return res;
-                })
-                .then((res) => {
-                    const blob = new Blob([res], { type: 'text/html' });
-                    const url = URL.createObjectURL(blob);
-                    iframe.src = url;
-                    placeholder.style.display = 'none';
-                    iframe.style.display = 'block';
-                    clear.style.display = 'block';
-                });
-        }
-    };
-    const handleClear = () => {
-        URL.revokeObjectURL(iframe.src);
-        fileList.forEach((file) => {
-            URL.revokeObjectURL(file.url);
-        });
-        fileList = [];
-        iframe.src = '';
-        uploader.value = '';
-        placeholder.style.display = 'block';
-        iframe.style.display = 'none';
-        clear.style.display = 'none';
-    };
-    document.addEventListener('beforeunload', handleClear);
-    clear.onclick = handleClear;
-})();
+import rulers from './url/rulers';
+import sketchMeasure from './url/sketchMeasure';
+import js from './url/js';
+import css from './url/css';
+import html from './url/html';
+import init from './init';
+
+const sketchMeasureCompare = {};
+
+if (!window.top.sketchMeasureCompare) {
+    window.top.sketchMeasureCompare = sketchMeasureCompare;
+
+    sketchMeasureCompare.__proto__.rulers = rulers;
+    sketchMeasureCompare.__proto__.sketchMeasure = sketchMeasure;
+    sketchMeasureCompare.__proto__.js = js;
+    sketchMeasureCompare.__proto__.css = css;
+    sketchMeasureCompare.__proto__.html = html;
+
+    sketchMeasureCompare.init = init;
+
+    if (process.env.NODE_ENV === 'development') {
+        console.log(sketchMeasureCompare);
+        sketchMeasureCompare.init({ zIndex: 10000 });
+    }
+}
---
+++ src/init.js
@@ -0,0 +1,121 @@
+export default ({ zIndex = 10000 } = {}) => {
+    const iframe = document.createElement('iframe');
+    const setStyle = (ele, obj) => {
+        Object.keys(obj).forEach((key) => {
+            ele.style[key] = obj[key];
+        });
+    };
+
+    setStyle(iframe, {
+        width: '100vw',
+        height: '100vh',
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        border: 'none',
+        zIndex: `${zIndex}`,
+        display: 'none',
+    });
+
+    const toggleBtnWrapper = document.createElement('div');
+    setStyle(toggleBtnWrapper, {
+        position: 'fixed',
+        zIndex: `${zIndex}`,
+        width: '100vw',
+        height: '0',
+        top: '0',
+        left: '0',
+    });
+
+    const toggleBtn = document.createElement('div');
+    setStyle(toggleBtn, {
+        position: 'fixed',
+        zIndex: `${zIndex}`,
+        right: '0',
+        bottom: '40px',
+        backgroundColor: '#04BE02',
+        padding: '2px 14px',
+        borderRadius: '3px',
+        color: '#fff',
+        height: '26px',
+        lineHeight: '26px',
+        fontSize: '16px',
+        userSelect: 'none',
+        webkitUserSelect: 'none',
+    });
+    toggleBtn.innerText = 'UI';
+
+    let shiftX = 0;
+    let shiftY = 0;
+    let isMoving = false;
+    let mouseDownX = 0;
+    let mouseDownY = 0;
+    let screenHeight = 0;
+    let screenWidth = 0;
+
+    toggleBtn.onclick = (e) => {
+        const { left, top } = toggleBtn.getBoundingClientRect();
+        const isClick =
+            Math.abs(left - mouseDownX) < 1 ||
+            Math.abs(top - mouseDownY) < 1 ||
+            !e?.isTrusted;
+        if (isClick) {
+            iframe.style.display =
+                iframe.style.display === 'none' ? 'block' : 'none';
+        }
+    };
+
+    const handleMouseDown = (e) => {
+        toggleBtnWrapper.style.height = '100vh';
+        const { left, top, width, height } = toggleBtn.getBoundingClientRect();
+        const { width: wrapperWidth, height: wrapperHeight } =
+            toggleBtnWrapper.getBoundingClientRect();
+        mouseDownX = left;
+        mouseDownY = top;
+        screenHeight = wrapperHeight;
+        screenWidth = wrapperWidth;
+        const clientX = e?.touches?.[0]?.clientX ?? e?.clientX;
+        const clientY = e?.touches?.[0]?.clientY ?? e?.clientY;
+        shiftX = clientX - mouseDownX - width;
+        shiftY = clientY - mouseDownY - height;
+        isMoving = true;
+    };
+
+    const handleMouseUp = () => {
+        toggleBtnWrapper.style.height = '0';
+        isMoving = false;
+    };
+
+    const handleMouseMove = (e) => {
+        e.preventDefault();
+        if (isMoving) {
+            const clientX = e?.touches?.[0]?.clientX ?? e?.clientX;
+            const clientY = e?.touches?.[0]?.clientY ?? e?.clientY;
+            toggleBtn.style.right = screenWidth - clientX + shiftX + 'px';
+            toggleBtn.style.bottom = screenHeight - clientY + shiftY + 'px';
+        }
+    };
+
+    toggleBtn.onmousedown = handleMouseDown;
+    toggleBtnWrapper.onmousemove = handleMouseMove;
+    toggleBtnWrapper.onmouseup = handleMouseUp;
+    toggleBtn.ontouchstart = handleMouseDown;
+    toggleBtnWrapper.ontouchmove = handleMouseMove;
+    toggleBtnWrapper.ontouchend = handleMouseUp;
+
+    toggleBtnWrapper.appendChild(toggleBtn);
+
+    fetch(window.top.sketchMeasureCompare.html)
+        .then((res) => res.text())
+        .then((html) => {
+            const blob = new Blob([html], { type: 'text/html' });
+            const url = URL.createObjectURL(blob);
+            iframe.src = url;
+            document.body.appendChild(iframe);
+            document.body.appendChild(toggleBtnWrapper);
+        });
+
+    document.addEventListener('beforeunload', () => {
+        URL.revokeObjectURL(iframe.src);
+    });
+};
