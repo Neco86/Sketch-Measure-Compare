0d158fa0e36a45115b4b3c68cbaf43ba31f36de8
Neco86 <33189641+Neco86@users.noreply.github.com>
2022-07-13 18:08:59 +0800

    Add files via upload

--- src/url/html.js
+++ src/url/html.js
@@ -1,3 +1,5 @@
+import html from './index.html';
+
 const func = () => {
     const script = document.createElement('script');
     script.src = window.top.sketchMeasureCompare.js;
@@ -13,31 +15,4 @@ const funcUrl = URL.createObjectURL(
     new Blob([`(${func})();`], { type: 'text/javascript' })
 );
 
-const html = `<!DOCTYPE html>
-<html lang="zh-CN">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Sketch Measure Compare</title>
-    </head>
-    <body>
-        <div id="placeholder">
-            <div>Please Choose UI Folder!</div>
-            <br/>
-            <input
-                type="file"
-                webkitdirectory
-                id="uploader"
-            />
-        </div>
-        <div id="clear">CLEAR UI</div>
-        <iframe id="iframe"></iframe>
-        <script src="${funcUrl}"></script>
-    </body>
-</html>
-`
-    .replace(/\n/g, '')
-    .replace(/\s+/g, ' ');
-
-export default URL.createObjectURL(new Blob([html], { type: 'text/html' }));
+export default URL.createObjectURL(new Blob([html.replace('$FUNC', funcUrl)], { type: 'text/html' }));
