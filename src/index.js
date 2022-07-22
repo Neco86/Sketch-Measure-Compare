2036c49dc14117705165b99f94b32219211d8eaf
Neco86 <33189641+Neco86@users.noreply.github.com>
2022-07-22 09:52:08 +0000

    Deploying to gh-pages from @ Neco86/Sketch-Measure-Compare@0375ee0deaff027436ec22dd3c4fa1d3327b49d7 🚀

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import rulers from './url/rulers';
+import sketchMeasure from './url/sketchMeasure';
+import index from './url/index';
+import css from './url/index.css';
+import html from './url/index.html';
+import tplIndex from './url/tpl';
+import tpl from './url/tpl.html';
+import init from './init';
+
+class SketchMeasureCompare {
+    constructor() {
+        this.init = init;
+    }
+}
+SketchMeasureCompare.prototype.rulers = rulers;
+SketchMeasureCompare.prototype.sketchMeasure = sketchMeasure;
+SketchMeasureCompare.prototype.index = index;
+SketchMeasureCompare.prototype.css = css;
+SketchMeasureCompare.prototype.html = html;
+SketchMeasureCompare.prototype.tplIndex = tplIndex;
+SketchMeasureCompare.prototype.tpl = tpl;
+
+if (!window.top.sketchMeasureCompare) {
+    const sketchMeasureCompare = new SketchMeasureCompare();
+    window.top.sketchMeasureCompare = sketchMeasureCompare;
+
+    if (process.env.NODE_ENV === 'development') {
+        console.log(sketchMeasureCompare);
+        sketchMeasureCompare.init({ zIndex: 10000 });
+    }
+}
+
+export default window.top.sketchMeasureCompare;
