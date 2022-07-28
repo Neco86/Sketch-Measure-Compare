d60ac060d547f3927fbf408e4c3398f6f34c7279
Neco86 <33189641+Neco86@users.noreply.github.com>
2022-07-28 11:14:07 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ const config = {
     plugins: isProduction
         ? [
               new CopyPlugin({
-                  patterns: ['README.md', 'package.json'],
+                  patterns: ['README.md', 'package.json', 'index.d.ts'],
               }),
           ]
         : [
