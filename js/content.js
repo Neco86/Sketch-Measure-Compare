1369601a6429af27b0c0b60b735c8107d195fcf2
Neco86 <33189641+Neco86@users.noreply.github.com>
2022-11-11 18:22:06 +0800

    Create content.js

---
+++ js/content.js
@@ -0,0 +1,26 @@
+const script = document.createElement('script');
+script.src = chrome.runtime.getURL('js/load.js');
+document.head.appendChild(script);
+
+const sendMsg2Load = ({ msg, payload }) => {
+    window.postMessage({
+        to: 'load',
+        msg,
+        payload,
+    });
+};
+
+const sendMsg2Popup = ({ msg, payload }) => {
+    chrome.runtime.sendMessage({ msg, payload });
+};
+
+chrome.runtime.onMessage.addListener((req) => {
+    sendMsg2Load(req);
+});
+
+window.addEventListener('message', (e) => {
+    const { msg, to, payload } = e.data || {};
+    if (to === 'content') {
+        sendMsg2Popup({ msg, payload });
+    }
+});
