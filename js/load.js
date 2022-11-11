52e66f4b79cbefb78a2be764c2ba1a114541bfdb
Neco86 <33189641+Neco86@users.noreply.github.com>
2022-11-11 18:22:55 +0800

    Add files via upload

---
+++ js/load.js
@@ -0,0 +1,52 @@
+(() => {
+    let loadPromise = null;
+
+    const sendMsgToContent = ({ msg, payload }) => {
+        if (typeof payload === 'object') {
+            payload = JSON.stringify(payload);
+        }
+        if (typeof payload === 'undefined') {
+            payload = '';
+        }
+        window.postMessage({ to: 'content', msg, payload });
+    };
+
+    window.addEventListener('message', (e) => {
+        const { msg, to, payload } = e.data || {};
+        if (to === 'load') {
+            if (msg === 'getConfig') {
+                sendMsgToContent({
+                    msg: 'getConfig',
+                    payload: window.top.sketchMeasureCompare?.config,
+                });
+            }
+            if (msg === 'config') {
+                const config = JSON.parse(payload);
+                if (loadPromise) {
+                    loadPromise.then(() => {
+                        console.log(
+                            '%c\ud83c\udf89 Sketch Measure Compare Config Success!',
+                            'color: #ffffff; background-color: #87d068; padding:4px 10px;'
+                        );
+                        if (window.top.sketchMeasureCompare?.config) {
+                            Object.keys(config).forEach((key) => {
+                                window.top.sketchMeasureCompare.config[key] =
+                                    config[key];
+                            });
+                        } else {
+                            window.top.sketchMeasureCompare.init(config);
+                        }
+                    });
+                }
+            }
+            if (msg === 'load' && !loadPromise) {
+                loadPromise = new Promise((resolve) => {
+                    const script = document.createElement('script');
+                    script.src = payload;
+                    document.head.appendChild(script);
+                    script.onload = resolve;
+                });
+            }
+        }
+    });
+})();
---
+++ js/popup.js
@@ -0,0 +1,96 @@
+const sendMsg2Content = ({ msg, payload }) => {
+    chrome.tabs.query(
+        {
+            active: true,
+            currentWindow: true,
+        },
+        (tabs) => {
+            if (/^http/.test(tabs[0]?.url)) {
+                if (typeof payload === 'object') {
+                    payload = JSON.stringify(payload);
+                }
+                if (typeof payload === 'undefined') {
+                    payload = '';
+                }
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    msg,
+                    payload,
+                });
+            }
+        }
+    );
+};
+
+const setConfig = (config) => {
+    zIndex.value = config.zIndex;
+    offsetY.value = config.offsetY;
+    enableDomRulers.checked = config.enableDomRulers;
+    enableTextReplace.checked = config.enableTextReplace;
+    chrome.storage.local.set({ sketchMeasureCompareConfig: config });
+};
+
+const DEFAULT_CONFIG = {
+    zIndex: 1000,
+    offsetY: 0,
+    enableDomRulers: false,
+    enableTextReplace: false,
+};
+
+const getConfig = (config) => {
+    if (config) {
+        return Promise.resolve(JSON.parse(config));
+    }
+    return new Promise((resolve) => {
+        chrome.storage.local.get(['sketchMeasureCompareConfig'], (res) => {
+            resolve(res.sketchMeasureCompareConfig || DEFAULT_CONFIG);
+        });
+    });
+};
+
+sendMsg2Content({
+    msg: 'getConfig',
+});
+
+chrome.runtime.onMessage.addListener((req) => {
+    const { msg, payload } = req || {};
+    if (msg === 'getConfig') {
+        getConfig(payload).then((config) => {
+            setConfig(config);
+            setTimeout(() => {
+                const style = document.createElement('style');
+                style.innerHTML = `
+                    .slider {
+                        transition: 0.1s;
+                    }
+                    .slider:before {
+                        transition: 0.1s;
+                    }
+                `;
+                document.head.appendChild(style);
+            }, 100);
+        });
+    }
+});
+
+fetch(chrome.runtime.getURL('package.json')).then(res => res.json()).then(res => {
+    version.innerHTML = `v${res.version}`;
+})
+
+apply.onclick = () => {
+    const config = {
+        zIndex: +zIndex.value || 10000,
+        offsetY: +offsetY.value || 0,
+        enableDomRulers: enableDomRulers.checked,
+        enableTextReplace: enableTextReplace.checked,
+    };
+    setConfig(config);
+    sendMsg2Content({
+        msg: 'load',
+        payload: chrome.runtime.getURL('index.min.js'),
+    });
+    sendMsg2Content({
+        msg: 'config',
+        payload: config,
+    });
+    window.close();
+};
